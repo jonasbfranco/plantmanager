c4ad53e24000d4b93640ad43f3d2c45d87a86fb8
jonasbfranco <jonasbfranco@hotmail.com>
2021-04-20 15:40:31 -0300

    Second Lesson

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
-import { Text, SafeAreaView, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
 
 
 export function Welcome() {
-    const [visible, setVisible] = useState(false);
-
-    function handleVisibility() {
-        setVisible(true)
-    }
-
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>
@@ -21,17 +14,18 @@ export function Welcome() {
                 forma fácil
             </Text>
 
-            {
-                visible &&
-                <Image source={wateringImg} style={styles.image} />
-            }
-
+            <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+            
             <Text style={styles.subtitle}>
                 Não esqueça mais de regar suas plantas. 
                 Nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <Button title= ">"/>
+            <TouchableOpacity style={styles.button} activeOpacity={0.7} >
+            <Text style={styles.buttonText}>
+                >
+            </Text>
+        </TouchableOpacity>
 
         </SafeAreaView>
     )
@@ -40,7 +34,7 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         alignItems: 'center'
     },
     title: {
@@ -56,6 +50,9 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         color: colors.heading
     },
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
     button: {
         backgroundColor: colors.green,
         justifyContent: 'center',
@@ -65,10 +62,6 @@ const styles = StyleSheet.create({
         height: 56,
         width: 56
     }, 
-    image: {
-        width: 292,
-        height: 284
-    },
     buttonText: {
         color: colors.white, 
         fontSize: 24
